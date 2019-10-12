7659d5e0814cd99f259b05a1a68c62f9712fdfa3
ericaguo <ericaguo@homeinns.com>
2019-10-12 18:07:40 +0800

    exec66.py

--- exec.leetcode/exec066.py
+++ exec.leetcode/exec066.py
@@ -1,28 +1,35 @@
-list1 = [9, 8, 8]
-# print(type(list1[2]))
-# print(type(list1))
+digits = [9, 9, 9]
+################一句必杀########################
+# print(type(digits[2]))
+# print(type(digits))
 
-# str1 = list(map(str, list1))
-# print(list1)
+# str1 = list(map(str, digits))
+# print(digits)
 # print(str1)
 # print(type(str1))
 
-# str2 = ''.join(map(str, list1))
+# str2 = ''.join(map(str, digits))
 # print(str2)
 # print('str2的类型为%s' % (type(str2)))
 
 # int2 = int(str2)+1
-# str3 =str(int(''.join(map(str, list1)))+1)
-# print(list(map(int,str(int(''.join(map(str, list1)))+1))))
-for i in range(len(list1)):
-    if list1[-1-i] == 9:
-        list1[-1-i] = 0
-        if -1-i == -len(list1):
-            list1[-1-i] =1
-            list1.append(0)
-            print(list1)
-    else:
-        list1[-1-i] +=1
-        print(list1) 
-        break
+# str3 =str(int(''.join(map(str, digits)))+1)
+# print(list(map(int,str(int(''.join(map(str, digits)))+1))))
+
+##################for#########################
+# for i in range(len(digits)):
+#     if digits[-1-i] == 9:
+#         digits[-1-i] = 0
+#         if -1-i == -len(digits):
+#             # digits[-1-i] =1
+#             # digits.append(0)
+#             digits.insert(0,1)
+#             print(digits)
+#     else:
+#         digits[-1-i] +=1
+#         print(digits) 
+#         break
+
+###############while#############################
+
 
