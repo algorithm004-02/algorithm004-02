eac43d24c5819ead0bc24e4968851600e0082670
“eericaguo <ericaguo@homeinns.com>
2019-10-15 22:31:34 +0800

    011

--- exec.leetcode/exec066.py
+++ exec.leetcode/exec066.py
@@ -1,4 +1,4 @@
-digits = [9, 9, 9]
+digits = [9, 1, 9]
 ################一句必杀########################
 # print(type(digits[2]))
 # print(type(digits))
@@ -17,18 +17,18 @@ digits = [9, 9, 9]
 # print(list(map(int,str(int(''.join(map(str, digits)))+1))))
 
 ##################for#########################
-# for i in range(len(digits)):
-#     if digits[-1-i] == 9:
-#         digits[-1-i] = 0
-#         if -1-i == -len(digits):
-#             # digits[-1-i] =1
-#             # digits.append(0)
-#             digits.insert(0,1)
-#             print(digits)
-#     else:
-#         digits[-1-i] +=1
-#         print(digits) 
-#         break
+for i in range(len(digits)):
+    if digits[-1-i] == 9:
+        digits[-1-i] = 0
+        if -1-i == -len(digits):
+            # digits[-1-i] =1
+            # digits.append(0)
+            digits.insert(0,1)
+            print(digits)
+    else:
+        digits[-1-i] +=1
+        print(digits) 
+        break
 
 ###############while#############################
 
