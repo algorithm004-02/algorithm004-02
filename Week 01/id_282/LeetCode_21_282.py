b253876cc9fc76612ab35ea7b6d9dd6bf878c551
王遥 <zhxzhs@gmail.com>
2019-10-19 23:46:35 +0800

    282 Week 01

---
+++ Week 01/id_282/LeetCode_21_282.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        newhead=ListNode(0)
+        now=newhead
+        while l1 and l2:
+            if l1.val<=l2.val:
+                now.next=l1
+                l1=l1.next
+            else:
+                now.next=l2
+                l2=l2.next
+            now=now.next
+        now.next =l2 if l2 is not None else l1
+        return newhead.next
