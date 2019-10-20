550efad776db0e2c49d940a18d1fff1049865492
“eericaguo <ericaguo@homeinns.com>
2019-10-20 22:09:06 +0800

    Week01

--- exec.leetcode/exec066.py
+++ exec.leetcode/exec066.py
@@ -1,4 +1,4 @@
-digits = [9, 1, 9]
+# 066. 加一
 ################一句必杀########################
 # print(type(digits[2]))
 # print(type(digits))
@@ -17,19 +17,42 @@ digits = [9, 1, 9]
 # print(list(map(int,str(int(''.join(map(str, digits)))+1))))
 
 ##################for#########################
-for i in range(len(digits)):
-    if digits[-1-i] == 9:
-        digits[-1-i] = 0
-        if -1-i == -len(digits):
-            # digits[-1-i] =1
-            # digits.append(0)
-            digits.insert(0,1)
-            print(digits)
-    else:
-        digits[-1-i] +=1
-        print(digits) 
-        break
+
+
+# def plusOne(digits):
+
+#     for i in range(len(digits)):
+#         if digits[-1-i] == 9:
+#             digits[-1-i] = 0
+#             if -1-i == -len(digits):
+#                 # digits[-1-i] =1
+#                 # digits.append(0)
+#                 digits.insert(0, 1)
+#         else:
+#             digits[-1-i] += 1
+#             break
 
 ###############while#############################
+# 执行用时 :44 ms
+def plusOne(digits):
+    j = len(digits)-1
+    while j >= 0:
+        if digits[j]+1 < 10:
+            digits[j] += 1
+            break
+        else:
+            digits[j] = 0
+        j -= 1
 
+    if digits[0] == 0:
+        digits.insert(0, 1)
+        return digits
+    else:
+        return digits
+
+
+#####################测试数据###################
+digits = [9, 9, 9]
+plusOne(digits)
+print(digits)
 
