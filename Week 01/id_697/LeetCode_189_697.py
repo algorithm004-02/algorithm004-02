aec5995ccdf78976d839b40fcde211e7204b48a1
zengkaimin <704571330@qq.com>
2019-10-22 14:54:50 +0800

    Week 01 update1

--- Week 01/id_697/LeetCode_189_697.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        k %= len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
