ffa4b971897a69becf69014619fc0dbd9e2395d9
viola.lu <viola.lu@vipshop.com>
2019-10-24 17:38:26 +0800

    577-Week 02

---
+++ Week 02/id_577/LeetCode_13_577.go
@@ -0,0 +1,43 @@
+var output [][]int
+
+
+func combine(n int, k int) [][]int {
+    
+    output = [][]int{}
+    
+    backtrack(1, []int{}, k , n)
+    
+    return output
+    
+    
+}
+
+func backtrack(first int, curr []int, k int,  n int) {
+    
+    if len(curr) == k {
+        
+        
+        
+        output = append(output, curr)
+        
+        
+        return 
+    }
+    
+    //put value in curr
+    for i := first; i<= n ; i ++  {
+        
+        curr = append(curr, i)
+        
+        backtrack(i + 1, curr, k, n)
+    
+        //remove last from curr.
+        curr = curr[:(len(curr) -1)]
+        
+    }
+    
+       
+    
+    
+    
+}
