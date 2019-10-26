585a4e14e039eaf113ee1622d8bbf5a8a6e48766
songzheng45 <songzheng45@126.com>
2019-10-27 01:09:10 +0800

    49. 字母异位词分组; 94. 二叉树的中序遍历

--- Week 02/id_107/LeetCode_49_107.cs
+++ Week 02/id_107/LeetCode_49_107.cs
@@ -32,4 +32,39 @@ public class Solution
     //     // 将分组后的异位词返回
     //     return new List<IList<string>>(mapping.Values);
     // }
+
+
+
+    // 解法 2:哈希表
+    // 核心思路: 计算每个单词里的每个字母出现的次数, 并将该字母出现的次数映射到哈希表里.
+    // 然后遍历哈希表后得到一个特殊字符串, 如果两个单词是异位词, 则对应的字符串相同.
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        if (strs == null || strs.Length == 0) return new List<IList<string>>();
+
+        int[] counter = new int[26];
+        Dictionary<string, IList<string>> ans = new Dictionary<string, IList<string>>();
+        foreach (string s in strs)
+        {
+            Array.Fill<int>(counter, 0);
+
+            foreach (char c in s) counter[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++)
+            {
+                sb.Append("#");
+                sb.Append(counter[i]);
+            }
+
+            string key = sb.ToString();
+            if (!ans.ContainsKey(key))
+            {
+                ans.Add(key, new List<string>());
+            }
+            ans[key].Add(s);
+        }
+
+        return new List<IList<string>>(ans.Values);
+    }
 }
