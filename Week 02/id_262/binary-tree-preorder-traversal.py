e5b0b787a97f9f549c77bd4fa4965b3a01f155cf
sxt <sxt@zionreality.com>
2019-10-27 21:33:03 +0800

    week 02 seven

--- Week 02/id_262/binary-tree-preorder-traversal.py
+++ Week 02/id_262/binary-tree-preorder-traversal.py
@@ -13,7 +13,7 @@ class Solution:
         if not root:
             return []
         l = []
-        l += self.inorderTraversal(root.left)
         l.append(root.val)
+        l += self.inorderTraversal(root.left)
         l += self.inorderTraversal(root.right)
         return l
