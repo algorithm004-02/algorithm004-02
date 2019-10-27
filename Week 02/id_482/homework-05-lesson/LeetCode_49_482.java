baf9b53a269b028da4f84303d3860e7672fae7c3
gd_space <gd_space@163.com>
2019-10-27 20:28:34 +0800

    add

--- Week 02/id_482/homework-05-lesson/LeetCode_49_482.java
+++ Week 02/id_482/homework-05-lesson/LeetCode_49_482.java
@@ -17,7 +17,7 @@ public class LeetCode_49_482 {
      * @param strs
      * @return
      */
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public List<List<String>> groupAnagrams_1(String[] strs) {
         if (strs == null || strs.length == 0)
             return new ArrayList<List<String>>();
         HashMap<String,List<String>> hashMap=new HashMap<>();
@@ -34,6 +34,37 @@ public class LeetCode_49_482 {
                 hashMap.put(key,tmpList);
             }
         }
+        return new ArrayList<List<String>>(hashMap.values());
+    }
+
+    /**
+     * 刷第二遍，哈希表的思想，
+     * 1、遍历string arr，对每一个string排序
+     * 2、new 一个临时 map
+     * map的key存放排序后的string，v 存放原 string
+     * @param strs
+     * @return
+     * 除了理解思想，还要熟悉代码的书写
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0)
+            return new ArrayList<List<String>>();
+        HashMap<String,List<String>> hashMap=new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+            char [] c= strs[i].toCharArray();
+            Arrays.sort(c);
+
+            String key=String.valueOf(c);
+            if (!hashMap.containsKey(key)){
+                List<String> list=new ArrayList<>();
+                list.add(strs[i]);
+                hashMap.put(key,list);
+            }else {
+                hashMap.get(key).add(strs[i]);
+            }
+        }
+        //需要注意这种写法
         return new ArrayList<List<String>>(hashMap.values());
     }
 
