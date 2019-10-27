f8ae0ab5813b7270235621b1259a098d7682edad
zhongxiaoyu <zhongxiaoyu@tuniu.com>
2019-10-27 18:03:25 +0800

    497-WEEK2 作业

---
+++ Week 02/id_497/LeetCode_590_497.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> integers = new ArrayList<>();
+        getNodeVal(root, integers);
+        return integers;
+    }
+
+    private void getNodeVal(Node node, List<Integer> integers) {
+        if (node == null) {
+            return;
+        }
+        if (node.children == null) {
+            integers.add(node.val);
+            return;
+        }
+        for (int i = 0; i < node.children.size(); i++) {
+            getNodeVal(node.children.get(i), integers);
+        }
+        integers.add(node.val);
+
+    }
+}
