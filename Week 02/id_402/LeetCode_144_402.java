0f1d1da596152b62daf4b4c161706cd372ed8201
SettleACG <settleacg@gmail.com>
2019-10-28 10:20:46 +0800

    week2

---
+++ Week 02/id_402/LeetCode_144_402.java
@@ -0,0 +1,15 @@
+﻿class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        DLR(root, list);
+        return list;
+    }
+
+    private void DLR (TreeNode node, List<Integer> list) {
+        if (node != null) {
+            list.add(node.val);
+            this.DLR(node.left, list);
+            this.DLR(node.right, list);
+        }
+    }
+}
