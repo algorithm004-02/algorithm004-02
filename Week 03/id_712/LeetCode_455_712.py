bdb7c02333125a10e2a86b7ee988f10e71060d2e
mashagua <993498039@qq.com>
2019-11-04 07:24:31 +0800

    add contents

---
+++ Week 03/id_712/LeetCode_455_712.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findContentChildren(self, g,s):
+        g.sort()
+        s.sort()
+        childi=0
+        cookiei=0
+        while cookiei<len(s) and childi<len(g)
+            if s[cookiei]>=g[childi]
+                childi+=1
+            cookiei+=1
+        return childi
