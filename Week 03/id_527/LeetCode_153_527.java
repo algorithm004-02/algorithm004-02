5ddceeba1274ff0a1e5a9fd8df124639e7cb9f28
zhao ying <joyin5344@gmail.com>
2019-11-03 22:04:11 +0800

    527-Week 03

---
+++ Week 03/id_527/LeetCode_153_527.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=153 lang=java
+ *
+ * [153] 寻找旋转排序数组中的最小值
+ */
+
+// @lc code=start
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
+// @lc code=end
