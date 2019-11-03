d9c0d9eb3c741cd34dd5c4598363269c3bbe0392
jyliuliu <1015376575@qq.com>
2019-11-03 21:07:05 +0800

    week 03

---
+++ Week 03/id_312/Leetcode-200-312.java
@@ -0,0 +1,54 @@
+给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+示例 1:
+输入:
+11110
+11010
+11000
+00000
+输出: 1
+
+示例 2:
+输入:
+11000
+11000
+00100
+00011
+输出: 3
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-islands
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+//应用深度优先的沉击法
+class Solution {
+  void dfs(char[][] grid, int r, int c) {
+    int nr = grid.length;
+    int nc = grid[0].length;
+    if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+      return;
+    }
+    grid[r][c] = '0';
+    dfs(grid, r - 1, c);
+    dfs(grid, r + 1, c);
+    dfs(grid, r, c - 1);
+    dfs(grid, r, c + 1);
+  }
+
+  public int numIslands(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
+    }
+
+    int nr = grid.length;
+    int nc = grid[0].length;
+    int num_islands = 0;
+    for (int r = 0; r < nr; ++r) {
+      for (int c = 0; c < nc; ++c) {
+        if (grid[r][c] == '1') {
+          ++num_islands;
+          dfs(grid, r, c);
+        }
+      }
+    }
+    return num_islands;
+  }
+}
