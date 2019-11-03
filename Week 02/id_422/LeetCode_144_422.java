b3476d1c3dcbe238c312560101f778e18fae0a81
G <yelangchuanshuo@geekcloud.com>
2019-11-03 21:03:45 +0800

    第二周 作业  有效的异位词

--- Week 02/id_422/LeetCode_144_422.java
+++ Week 02/id_422/LeetCode_144_422.java
@@ -8,29 +8,41 @@
  * }
  */
 class Solution {
-    List<Integer> list = new ArrayList();
-    
-    public List<Integer> preorderTraversal(TreeNode root) {
-   
-        //结束条件
-        if(root == null){
-            return list;
-        }
-        
-        list.add(root.val);
-        
-		TreeNode leftTree = root.left;
-		if(leftTree != null)
-		{
-			preorderTraversal(leftTree);
+	List<Integer> list = new ArrayList();
+
+	public List<Integer> preorderTraversal(TreeNode root) {
+
+		List<Integer> result = new LinkedList();
+
+		//recursion(root,result);
+		iteration(root,result);
+		return result;
+
+	}
+
+	void recursion(TreeNode root,List<Integer> result){
+
+		if(root == null) return;
+		result.add(root.val);
+		recursion(root.left,result);
+		recursion(root.right,result);
+
+	}
+
+	void iteration(TreeNode root,List<Integer> result){
+
+		if(root == null) return ;
+
+		Stack<TreeNode> stack = new Stack();
+		TreeNode cur;
+
+		stack.push(root);
+		while(!stack.isEmpty()){
+			cur = stack.pop();
+			result.add(cur.val);
+			if(cur.right != null) stack.push(cur.right);
+			if(cur.left != null) stack.push(cur.left);
+
 		}
-        
-		TreeNode rightTree = root.right;
-		if(rightTree != null)
-		{
-			preorderTraversal(rightTree);
-		}
-        
-        return list;
-    }
+	}
 }
