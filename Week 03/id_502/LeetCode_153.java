8a0725a87e7b084a9b8bf09b8ffc0082762748b7
wangmy <wmy19890301@163.com>
2019-11-04 08:20:25 +0800

    Update LeetCode_153.java

--- Week 03/id_502/LeetCode_153.java
+++ Week 03/id_502/LeetCode_153.java
@@ -6,7 +6,7 @@ class Solution {
         
         int mid = nums.length/2;
         if(nums.length <= (mid + 1)) {
-            //<=两个
+            //=两个
             if (nums[mid] > nums[mid - 1]) {
                 return nums[mid - 1];
             }
@@ -17,8 +17,6 @@ class Solution {
                 && nums[mid] < nums[mid + 1]
                 && nums[mid] > nums[mid - 1]) {
                 return nums[0];
-            } else {
-                
             }
             return nums[mid];
         }
