e46dc02ce12349455fbe364ae9e6519473c0fb9c
GaoWenJing0508 <55942146+GaoWenJing0508@users.noreply.github.com>
2019-11-05 18:36:42 +0800

    Create LeetCode_94.py

---
+++ Week 02/id_337/LeetCode_94.py
@@ -0,0 +1,18 @@
+class Solution {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = root;
+            while (cur != null || !stack.isEmpty()) {
+                if (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                } else {
+                    cur = stack.pop();
+                    list.add(cur.val);
+                    cur = cur.right;
+                }
+            }
+            return list;
+        }
+    }
