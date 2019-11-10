eaae2854f6dcfccbe349cca3add15755876ce9e1
XiaoDhuang <1156299758@qq.com>
2019-11-10 20:31:09 +0800

    update homework

--- Week 02/id_002/leetCode_144_002.js
+++ Week 02/id_002/leetCode_144_002.js
@@ -15,3 +15,28 @@ var inorderTraversal = function(root) {
     
     return res;
 };
+
+/**
+ * 迭代方式 前序遍历二叉树
+ * 时间复杂度O(n) 空间复杂度O(n)
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let stack =  [];
+    let node = root;
+    let res = []
+    
+    while(stack.length || node) {
+        while(node) {
+            res.push(node.val);
+            stack.push(node);
+            node = node.left;
+        }
+        
+        node = stack.pop();
+        node = node.right;
+    }
+    
+    return res;
+};
