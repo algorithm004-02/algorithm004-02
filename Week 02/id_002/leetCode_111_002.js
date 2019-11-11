b6212cc425ffaaca1e7230cd32911c5f6a84c5b1
XiaoDHuang <1156299758@qq.com>
2019-11-11 10:55:51 +0800

    Create leetCode_111_002.js

---
+++ Week 02/id_002/leetCode_111_002.js
@@ -0,0 +1,33 @@
+/** 111. 二叉树的最小深度 **/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * 深度优先 递归版
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var minDepth = function(root) {
+    return dfs(root, 0);
+    
+    function dfs(node, dep) {
+        if (!node) return dep;
+        
+        let leDep = dfs(node.left, dep + 1);
+        let riDep = dfs(node.right, dep + 1);
+        
+        if (node.left && node.right) {
+            return Math.min(leDep, riDep);
+        } else if (node.left || node.right) {
+            return node.left ? leDep : riDep;
+        } else {
+            return dep + 1;
+        }
+    }
+};
+ 
