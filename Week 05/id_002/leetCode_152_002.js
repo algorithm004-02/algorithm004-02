d9f7a93e3afbcc183f5f48c4d6c60fbfae311b2d
XiaoDHuang <1156299758@qq.com>
2019-11-15 16:36:14 +0800

    Update leetCode_152_002.js

--- Week 05/id_002/leetCode_152_002.js
+++ Week 05/id_002/leetCode_152_002.js
@@ -1,18 +1,29 @@
 /**
+1. 暴力
+2. DP：
+    a. 分治（子问题)
+        res = [number[i], number[i] * a[i - 1][0], number[i] * a[i - 1][1]]
+        a[i] = [Min(res), Max(res)]
+    b. 状态数组定义 f[i] = [minVal, maxVal]
+    c. DP方程 
+         res = [number[i], number[i] * f[i - 1][0], number[i] * f[i - 1][1]]
+         f[i] = [Min(res), Max(res);
+ */
+
+/**
  * @param {number[]} nums
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let max = - Number.POSITIVE_INFINITY;
-    let dpMax = 1;
     let dpMin = 1;
+    let dpMax = 1;
+    let max = - Number.POSITIVE_INFINITY;
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] < 0) [dpMax, dpMin] = [dpMin, dpMax];
-
-        dpMax = Math.max(dpMax * nums[i], nums[i]);
-        dpMin = Math.min(dpMin * nums[i], nums[i]);
-
+    for (let i = 0; i < nums.length; i ++) {
+        let res = [nums[i], nums[i] * dpMin, nums[i] * dpMax];
+        dpMin = Math.min(...res);
+        dpMax = Math.max(...res);
+        
         max = Math.max(dpMax, max);
     }
 
