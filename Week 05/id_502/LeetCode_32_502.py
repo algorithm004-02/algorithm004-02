b69e2d2fbcb190eb46458459ccc5469980c0146a
wmy <mingyou@mafengwo.com>
2019-11-18 00:07:53 +0800

    最长有效括号

---
+++ Week 05/id_502/LeetCode_32_502.py
@@ -0,0 +1,17 @@
+
+#DPs
+def longestValidParentheses(s: str) -> int:
+        n = len(s)
+        if n <= 1: return 0
+        dp = [0] * n
+        res = 0
+        for i in range(n):
+            if i>0 and s[i] == ")":
+                if  s[i - 1] == "(":
+                    dp[i] = dp[i - 2] + 2
+                elif s[i - 1] == ")" and i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == "(":
+                    dp[i] = dp[i - 1] + 2 + dp[i - dp[i - 1] - 2]
+            res = max(res,dp[i])
+        return res
+
+longestValidParentheses('((())))))')
