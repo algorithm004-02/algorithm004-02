446e53b1d7b0b4bc80302adf9ee758b4f498484b
wmy <mingyou@mafengwo.com>
2019-11-18 14:32:38 +0800

    optimize

--- Week 05/id_502/LeetCode_32_502.py
+++ Week 05/id_502/LeetCode_32_502.py
@@ -1,6 +1,33 @@
 
-#DPs
-def longestValidParentheses(s: str) -> int:
+#最长有效括号
+# 暴力解决：每次取偶数个，时间复杂度：n^2
+
+def isValidParentheses(s: str) -> bool:
+    stack = []
+    for i in range(len(s)):
+        if s[i] == "(":
+            stack.append("(")
+        elif len(stack) > 0:
+            stack.pop()
+        else:
+            return False
+    return (len(stack) == 0)
+
+def longestValidParentheses_recursion(s: str) -> int:
+        if not s or len(s) <= 0:
+            return 0
+        res = 0
+        
+        for i in range(len(s)):
+            for j in range(i+2,len(s)+1, 2):
+                if (isValidParentheses(s[i:j])):
+                    res = max(res, j-i)
+
+        return res
+
+
+#DP 解决
+def longestValidParentheses_dp(s: str) -> int:
         n = len(s)
         if n <= 1: return 0
         dp = [0] * n
@@ -14,4 +41,5 @@ def longestValidParentheses(s: str) -> int:
             res = max(res,dp[i])
         return res
 
-longestValidParentheses('((())))))')
+print("recursion:"+str(longestValidParentheses_recursion("(())()")))
+print("dp:"+str(longestValidParentheses_dp("(())()")))
