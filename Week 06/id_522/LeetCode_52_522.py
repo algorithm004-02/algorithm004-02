dc2e62da53a1d3dc34c83acbc3a38b93e1da81c5
lsx222 <plzyyxxk@sina.com>
2019-11-20 20:56:40 +0800

    11-20-2
    11-20-2

---
+++ Week 06/id_522/LeetCode_52_522.py
@@ -0,0 +1,34 @@
+#难度大,,对撇捺的理解，很重要
+class Solution:
+    def solveNQueens(self,n):
+        r=[]
+        def dfs(q,cha,he):
+            y=len(q)
+            if y==n: #8
+                a=["."*i +"Q"+"."*(n-1-i) for i in q]
+                r.append(a)
+                return
+            for x in range(n):
+                if x not in q and y-x not in cha and y+x not in he:
+                    dfs(q+[x],cha+[y-x],he+[y+x])
+        dfs([],[],[])
+        return len(r)
+'''
+class Solution:
+    def solveNQueens(self, n):
+        res=[]
+        
+        def dfs(queens,cha,he):
+            y=len(queens)  #y轴，就是行号
+            if y==n: #8
+                a=["."*i +"Q"+"."*(n-1-i) for i in queens]
+                res.append(a)
+                return
+            
+            for x in range(n):
+                if x in queens or y-x in cha or y+x in he:continue
+                dfs(queens+[x],cha+[y-x],he+[y+x])
+            #return
+        dfs([],[],[])
+        return len(res)
+'''
