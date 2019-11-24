29161b4559ceeb780cb0a7cc2813e169118f2891
Eden Liao <eden@eden.local>
2019-11-25 00:03:15 +0800

    无

---
+++ Week 06/id_377/208.implement-trie-prefix-tree.go
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=208 lang=golang
+ *
+ * [208] Implement Trie (Prefix Tree)
+ *
+
+ */
+
+// @lc code=start
+type Trie struct {
+    
+}
+
+
+/** Initialize your data structure here. */
+func Constructor() Trie {
+    
+}
+
+
+/** Inserts a word into the trie. */
+func (this *Trie) Insert(word string)  {
+    
+}
+
+
+/** Returns if the word is in the trie. */
+func (this *Trie) Search(word string) bool {
+    
+}
+
+
+/** Returns if there is any word in the trie that starts with the given prefix. */
+func (this *Trie) StartsWith(prefix string) bool {
+    
+}
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
+// @lc code=end
+
---
+++ Week 06/id_377/212.word-search-ii.go
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=212 lang=golang
+ *
+ * [212] Word Search II
+ * 
+ */
+
+// @lc code=start
+func findWords(board [][]byte, words []string) []string {
+    
+}
+// @lc code=end
+
