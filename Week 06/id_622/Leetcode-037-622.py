b56b195a71d6ece7fcacaac8b5b0c8b3b7ac21a8
patrick.chen <developer@infiai.com>
2019-11-24 22:42:49 +0800

    Modify 037, and add new solution

--- Week 06/id_622/Leetcode-037-622.py
+++ Week 06/id_622/Leetcode-037-622.py
@@ -26,7 +26,9 @@ Note:
 链接：https://leetcode-cn.com/problems/sudoku-solver
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
-"""
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """    
         Do not return anything, modify board in-place instead.
         """
         """
@@ -68,3 +70,47 @@ Note:
                 box[box_index].add(val)
             return False
         backtrace()
+
+    
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        https://leetcode.com/problems/sudoku-solver/discuss/15752/Straight-Forward-Java-Solution-Using-Backtracking
+        """
+        if not board or len(board) == 0:
+            return
+        self.solve(board)
+        
+    def solve(self, board):
+        """
+        rtype: boolean
+        """
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == '.':
+                    for c in "123456789":
+                        if self.isValid(board, i, j, c):
+                            board[i][j] = c
+                            # If it's the solution return true
+                            if self.solve(board): 
+                                return True
+                            # Otherwise go back
+                            else:
+                                board[i][j] = '.'
+                    return False
+        return True
+        
+    def isValid(self, board, x, y, c):
+        for i in range(9):
+            if board[i][y] == c: 
+                return False
+        for j in range(9):
+            if board[x][j] == c:
+                return False
+        for i in range(3):
+            for j in range(3):
+                if board[(x//3)*3 + i][(y//3)*3 + j] == c:
+                    return False
+                    
+        return True
+        
