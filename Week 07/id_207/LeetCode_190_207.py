d9b3a4bed80d3fd40cbc60c4b3892b1214a85f39
Sodbilig <sodbilig79@gmail.com>
2019-12-01 21:52:45 +0800

    Week07_207

--- Week 07/id_207/LeetCode_190_207.py
+++ Week 07/id_207/LeetCode_190_207.py
@@ -5,8 +5,9 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
         for i in range(32):
+            # 将n的最低位放到res中
+            # 将res左移，n右移
             res <<= 1
-            
             res += n & 1
             n >>= 1
         return res
