fcbe61592d2ada1179d7bd0fcde0525eac6e02b4
lsx222 <plzyyxxk@sina.com>
2019-12-04 09:18:54 +0800

    124

--- Week 08/id_522/LeetCode_387_522.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        dd , a = collections.Counter(s) , 0
-        for i in s:
-            if dd[i] == 1:
-                return a
-            else:
-                a += 1
-        return -1
