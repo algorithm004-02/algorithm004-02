3f5fe61725ce6cddb90ae17571686574a1508f2d
XiaoDhuang <1156299758@qq.com>
2019-12-08 19:08:01 +0800

    update week 08 homework

--- Week 08/id_002/leetCode_32_002.js
+++ Week 08/id_002/leetCode_32_002.js
@@ -40,3 +40,24 @@ var longestValidParentheses = function(s) {
     return maxLen;
 };
 
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let n = s.length;
+    let res = "";
+    let dp = Array.from(Array(n), () => Array(n).fill(0));
+
+    for(let i = n - 1; i >= 0; i--) {
+        for (let j = i; j < n; j ++) {
+            dp[i][j] = (s[i] === s[j]) && (j - i < 2 || dp[i + 1][j - 1]);
+
+            if (dp[i][j] && (j - i + 1 > res.length)) {
+                res = s.substring(i, j + 1);
+            }
+        }
+    }
+
+    return res;
+};
