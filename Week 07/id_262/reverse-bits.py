6bc5ed6d6767ee9a053af109e5814d9e0516b7af
sxt <sxt@zionreality.com>
2019-12-08 21:00:25 +0800

    262-Week 08

--- Week 07/id_262/reverse-bits.py
+++ Week 07/id_262/reverse-bits.py
@@ -1,46 +1,9 @@
-# class Solution:
-#     def reverseBits(self, n):
-#         res, mask = 0, 1
-#
-#         for i in range(32):
-#             if n & mask:
-#                 res |= 1 << (31 - i)
-#             mask <<= 1
-#         return res
-
-# import re
-# class Solution:
-#     def myAtoi(self, s: str) -> int:
-#         return max(min(int(*re.findall('^[\+\-]?\d+', s.lstrip())), 2**31 - 1), -2**31)
-
-
 class Solution:
-    def reverseStr(self, s: str, k: int) -> str:
+    def reverseBits(self, n):
+        res, mask = 0, 1
 
-        before = ""
-        after = ""
-
-        mod = len(s) % (2 * k)
-        handle_str = s
-        if 2 * k > len(s):
-            if mod <= k:
-                after = s[-mod:][::-1]
-            elif k < mod < 2 * k:
-                after = s[-mod: -mod + k][::-1] + s[-mod + k:]
-            return after
-        elif 2 * k < len(s):
-            if mod > 0:
-                if mod < k:
-                    after = s[-mod:][::-1]
-                elif k < mod < 2 * k:
-                    after = s[-mod: -mod + k][::-1] + s[-mod + k:]
-                handle_str = s[:-mod]
-
-        for i in range(int(len(handle_str) / (2 * k))):
-            per_str = handle_str[i * 2 * k:2 * k * i + 2 * k]
-            before += per_str[:k][::-1] + per_str[k:]
-        return before + after
-
-
-s = Solution()
-print(s.reverseStr("abcd", 3))
+        for i in range(32):
+            if n & mask:
+                res |= 1 << (31 - i)
+            mask <<= 1
+        return res
