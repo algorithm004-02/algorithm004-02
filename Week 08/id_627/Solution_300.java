2faf08477b89043fd6dcb852d6eccdb98e29c869
wnwangnan <1792339953@qq.com>
2019-12-09 21:16:20 +0800

    627-week8

---
+++ Week 08/id_627/Solution_300.java
@@ -0,0 +1,18 @@
+class Solution_300 {
+     public int lengthOfLIS(int[] nums) {
+        return lengthofLIS(nums, Integer.MIN_VALUE, 0);
+    }
+
+    public int lengthofLIS(int[] nums, int prev, int curpos) {
+        if (curpos == nums.length) {
+            return 0;
+        }
+        int taken = 0;
+        if (nums[curpos] > prev) {
+            taken = 1 + lengthofLIS(nums, nums[curpos], curpos + 1);
+        }
+        int nottaken = lengthofLIS(nums, prev, curpos + 1);
+        return Math.max(taken, nottaken);
+    }
+
+}
