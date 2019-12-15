c5d084bd7a9e33e8846929e0bd1dd3ca7db67d44
liangjieheng <ljh01124578@gmail.com>
2019-12-15 23:45:40 +0800

    Week8

---
+++ Week 08/id_137/LeetCode_1143_137.java
@@ -0,0 +1,10 @@
+public class LeetCode_1143_137 {
+    public int longestCommonSubsequence(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i < s1.length(); ++i)
+            for (int j = 0; j < s2.length(); ++j)
+                if (s1.charAt(i) == s2.charAt(j)) dp[i + 1][j + 1] = 1 + dp[i][j];
+                else dp[i + 1][j + 1] =  Math.max(dp[i][j + 1], dp[i + 1][j]);
+        return dp[s1.length()][s2.length()];
+    }
+}
