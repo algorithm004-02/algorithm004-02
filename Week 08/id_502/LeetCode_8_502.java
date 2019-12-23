d0bab405915a31a981564b8ca051596aefa8c682
wmy <mingyou@mafengwo.com>
2019-12-23 17:10:46 +0800

    502 - Week 08

---
+++ Week 08/id_502/LeetCode_8_502.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int myAtoi(String str) {
+        str = str.trim();
+        if (str == null || str.length() == 0)
+            return 0;
+
+        char firstChar = str.charAt(0);
+        int sign = 1;
+        int start = 0;
+        long res = 0;
+        if (firstChar == '+') {
+            sign = 1;
+            start++;
+        } else if (firstChar == '-') {
+            sign = -1;
+            start++;
+        }
+
+        for (int i = start; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return (int) res * sign;
+            }
+            res = res * 10 + str.charAt(i) - '0';
+            if (sign == 1 && res > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+            if (sign == -1 && res > Integer.MAX_VALUE)
+                return Integer.MIN_VALUE;
+        }
+        return (int) res * sign;
+    }
+}
---
+++ Week 08/id_502/NOTE.md
@@ -0,0 +1,4 @@
+# NOTE
+字符串是否可变：
+Java/Python/Go:字符串是不可变，每次修改字符串，都会生成一个新的字符串，安全性更高
+C++/ruby/PHP/swift:字符串可变，如果需要使用不可变，C++使用const; swift 使用let
